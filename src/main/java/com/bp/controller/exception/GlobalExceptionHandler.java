0738655a5f0cceb7d1471d3323a3534b029bc85b
baopan <baopan@gaosiedu.com>
2017-09-06 10:02:36 +0800

    change more ,,,,,

---
+++ src/main/java/com/bp/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.bp.controller.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Administrator on 2017/9/6.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseBody
+    public String handleHttpMessageNotReadableException(HttpMessageNotReadableException e){
+        logger.error("handleHttpMessageNotReadableException : " + e.getMessage() , e);
+        return "参数解析失败";
+    }
+}
